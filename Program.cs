2d41c0e4198d81fc3b2ea1efe015c52b50a89491
yazeedAlsubaie <38755811+yazeedAlsubaie@users.noreply.github.com>
2021-03-28 16:07:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace myproject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome!");
+            List<Student> students = new List<Student>();
+            int leave = 1;
+            do
+            {
+                var student = new Student();
+                Console.WriteLine("please enter student name");
+                student.name = Console.ReadLine();
+                Console.WriteLine("please enter student grade");
+                student.grade = int.Parse(Console.ReadLine());
+                Console.WriteLine("student information has been added");
+                Console.WriteLine("if you want to add another stuednt enter 1 if not enter 0");
+                students.Add(student);
+                leave = int.Parse(Console.ReadLine());
+
+            } while (leave == 1);
+
+            for (int i = 0; i < students.Count; i++) {
+                students[i].print();
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myproject
+{
+    class Student
+    {
+        public String name;
+        public double grade;
+
+        public Student() { }
+        public Student(String name, double grade) {
+            this.name = name;
+            this.grade = grade;
+            
+        }
+
+        public void print() {
+            Console.WriteLine("student name is {0}" + " and student grade is {1}", name,grade);
+            if (grade>60) {
+                Console.WriteLine("student has passed");
+            }
+            else
+            {
+                Console.WriteLine("student has Failed ");
+            }
+        }
+            
+    }
+}
